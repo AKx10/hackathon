c93c7f10d5f5fc05685a9d0b195900ade4e9a8f8
Akshay K <akshayk@Akshays-MacBook-Air.local>
2024-01-23 18:37:15 +0530

    bg black

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
   localStorage.setItem("user", userString);
 
   return (
-    <main className="flex flex-1 h-[100vh] w-[100vw] flex-col items-center justify-center p-24">
+    <main className="flex flex-1 h-[100vh] w-[100vw] flex-col items-center justify-center p-24 bg-black">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
