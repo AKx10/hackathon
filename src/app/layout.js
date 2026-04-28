ef960eb0e219f92075a14b1ebf3853344012a4b5
AKx10 <akshay@thealteroffice.com>
2026-04-28 18:19:36 +0530

    load sdk as soon as possible

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Script async src="/advertiser-sdk.js"></Script>
+        <Script src="/advertiser-sdk.js" strategy="beforeInteractive" />
       </body>
     </html>
   );
